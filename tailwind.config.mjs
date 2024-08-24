cff727611c2a86962682710ec233575870e0803b
Bartosz Trusiński <bartosztrusinski@gmail.com>
2024-08-24 17:34:36 +0200

    style weekday result text

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -2,7 +2,13 @@
 export default {
   content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
   theme: {
-    extend: {},
+    extend: {
+      backgroundImage: {
+        /* made at https://learnui.design/tools/gradient-generator.html */
+        gradient:
+          'linear-gradient(90deg, #003f5b, #2b4b7d, #5f5195, #98509d, #cc4c91, #f25375, #ff6f4e, #ff9913); ',
+      },
+    },
   },
   plugins: [],
 };
